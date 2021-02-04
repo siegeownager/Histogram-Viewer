1f164fefda0d8f83cf53f89a3e8288162d1efe38
justeeson <justeeson.1@osu.edu>
2021-02-03 20:20:23 -0500

    Added a new function create_buckets to create the RGB arrays

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ def grab_dimensions(image):
     width, height = image.size
     return width, height
 
+def create_buckets(image, width, height):
+    for i in range(height):
+        for j in range(width):
+            im = image.getpixel((j, i))
+            print(im)
+
+
 
 def main():
     image = open_image()
     width, height = grab_dimensions(image)
-    print(width)
-    print(height)
+    create_buckets(image, width, height)
 
 
 if __name__ == "__main__":

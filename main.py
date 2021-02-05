66961a6b33254bb2202852093abbae7f10aac886
justeeson <justeeson.1@osu.edu>
2021-02-04 22:37:16 -0500

    Now able to accurate display the R, G and B histograms of an image in the directory.

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ def open_image():
     return Image.open('cat.jpg')
 
 
+# Function to plot the graph
+def plot_graph(color_array, color):
+    x_array = list(range(256))
+    plt.plot(x_array, color_array, color)
+
+
+# Function to display the graph
+def display_plots():
+    plt.show()
+
+
 # Function to grab the dimensions of the image
 def grab_dimensions(image):
     width, height = image.size
@@ -31,9 +42,8 @@ def test_array(array):
     return sum_val
 
 
-def create_buckets(image, width, height):
-    R_array, G_array, B_array = initialize_color_arrays()
-
+# Function to create arrays with frequencies of each R, G, B value
+def create_buckets(image, width, height, R_array, G_array, B_array):
     for i in range(height):
         for j in range(width):
             red_val, green_val, blue_val = image.getpixel((j, i))
@@ -41,15 +51,20 @@ def create_buckets(image, width, height):
             G_array[green_val] += 1
             B_array[blue_val] += 1
 
-    plt.hist(R_array)
-    plt.show()
+    return R_array, G_array, B_array
 
 
 def main():
     image = open_image()
     width, height = grab_dimensions(image)
-    create_buckets(image, width, height)
-    initialize_color_arrays()
+    R_array, G_array, B_array = initialize_color_arrays()
+    create_buckets(image, width, height, R_array, G_array, B_array)
+
+    plot_graph(R_array, 'red')
+    plot_graph(G_array, 'green')
+    plot_graph(B_array, 'blue')
+
+    display_plots()
 
 
 if __name__ == "__main__":

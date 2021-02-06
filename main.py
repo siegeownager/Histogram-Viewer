60d02b7787f896aa5b7073f1a34ef6b66882aaae
justeeson <justeeson.1@osu.edu>
2021-02-06 17:51:28 -0500

    Added a new prompt that lets you select the image file for which you want to create the histogram.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import numpy
-from PIL import Image
+import PIL
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import *
+from tkinter.filedialog import askopenfilenames, askdirectory, askopenfilename
+from tkinter.messagebox import showerror, showinfo
+
+WINDOW_SIZE = '300x300'
 
 
 # Function to open and return the image object
 def open_image():
-    return Image.open('cat.jpg')
+    image_file = askopenfilename(filetypes=(("Image Files", "*.jpg *.png"),))
+    return PIL.Image.open(image_file)
 
 
 # Function to plot the graph
@@ -54,7 +61,42 @@ def create_buckets(image, width, height, R_array, G_array, B_array):
     return R_array, G_array, B_array
 
 
+# This function is for generating the graphical window
+def generate_ui():
+
+    root = Tk()
+
+    root.geometry(WINDOW_SIZE)
+
+    frame1 = Frame(root)  # Image Select button
+    frame2 = Frame(root)  # Convert button
+
+    root.title("Image Histogram Viewer")
+
+    # Button that executes the function to load the log file(s)
+    button_default_text = StringVar()
+    button_default_text.set("Select Image")
+
+    select_button = Button(frame1, textvariable=button_default_text, command=open_image, width=22)
+    select_button.pack()
+
+    # Scroll bar and the list box for displaying the timezones
+    scrollbar = Scrollbar(frame2)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+
+
+    # Button that calls the conversion function
+    convert_button = Button(frame2, text="Convert", command=open_image, width=22)
+    convert_button.pack()
+
+
+    frame1.pack(padx=10, pady=10)
+    frame2.pack(padx=1, pady=1)
+
+
 def main():
+    #generate_ui()
     image = open_image()
     width, height = grab_dimensions(image)
     R_array, G_array, B_array = initialize_color_arrays()
@@ -65,6 +107,7 @@ def main():
     plot_graph(B_array, 'blue')
 
     display_plots()
+
 
 
 if __name__ == "__main__":
